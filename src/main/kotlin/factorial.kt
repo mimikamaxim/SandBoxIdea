8c08c804167de368c2ac75d0b219bcb31128c6a4
mimika <mi@mi>
2022-11-11 17:20:27 +0500

    add wrappers and printCounter

--- src/main/kotlin/factorial.kt
+++ src/main/kotlin/factorial.kt
@@ -1,18 +1,14 @@
+
+var counter:Int = 0
 fun main(){
     println("Program start...")
     val number = 16
-    println("The fibonacci of number $number is ${fibonacciSimple(number)}")
-    println("The fibonacci of number $number is ${fibonacciRecursive(number)}")
-    println("counter $counter")
+    fibByRecursiveWrapper(number)
+    fibByStepsWrapper(number)
 }
-
-var counter:Int = 0
-
-fun fibonacciSimple (n: Int):Int{
+fun fibonacciBySteps (n: Int):Int{
     if (n==0) return 0
     if (n==1) return 1
-    //counter for steps
-    var steps = 0
     //define data for first step
     var nMns2 = 0
     var nMns1 = 1
@@ -25,17 +21,31 @@ fun fibonacciSimple (n: Int):Int{
         nMns2 = nMns1
         nMns1 = res
         //increase steps counter
-        steps++
+        counter++
     }
-    println("The simple way fibonacci calculated by $steps steps")
     return res
 }
-
-fun fibonacciRecursive (n: Int):Int{
+fun fibByStepsWrapper (n: Int) {
+    counter =0
+    println("The fibonacci calculated by steps of number $n is ${fibonacciBySteps(n)}")
+    printCounter()
+}
+fun fibonacciByRecursive (n: Int):Int{
     counter++
     if (n==0) return 0
     if (n==1) return 1
-    val a = fibonacciRecursive(n-2)
-    val b = fibonacciRecursive(n-1)
+    val a = fibonacciByRecursive(n-2)
+    val b = fibonacciByRecursive(n-1)
     return a+b
+}
+
+fun fibByRecursiveWrapper (n: Int){
+    counter = 0
+    println("The fibonacci calculated recursively of number $n is ${fibonacciByRecursive(n)}")
+    printCounter()
+}
+
+fun printCounter (){
+    println("Iteration count is $counter")
+    println("--------------------------------<")
 }
